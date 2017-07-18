28c18aa09938dd6bed7e394ea5250289119e2e7d
themonster2015 <quynhyen.vothi@gmail.com>
2017-07-18 20:30:44 +0200

    added authentication with Devise

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,18 @@
+class OrdersController < ApplicationController
+before_filter :authenticate_user!
+	def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+  
+end
