6e183a0c64b09079d2f336d622df5892cd084ce8
themonster2015 <quynhyen.vothi@gmail.com>
2017-07-19 01:28:21 +0200

    added image zoomin featre for product page

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,18 +1,16 @@
+$(".alert").delay(2000).fadeOut(3000);
 $(document).on('turbolinks:load', function(){
-  $('.rating').raty({
-    starHalf: '<%= asset_path 'star-half.png' %>',
-    starOff: '<%= asset_path 'star-off.png' %>',
-    starOn: '<%= asset_path 'star-on.png' %>',
-    scoreName: 'comment[rating]'
-  });
-
-  $('.rated').raty({
-    starHalf: '<%= asset_path 'star-half.png' %>',
-    starOff: '<%= asset_path 'star-off.png' %>',
-    starOn: '<%= asset_path 'star-on.png' %>',
-    readOnly: true,
-    score: function() {
-      return $(this).attr('data-score');
-    }
-  });
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+      readOnly: true,
+      score: function() {
+        return $(this).attr('data-score');
+      }
+    });
+});
+$(document).on('turbolinks:load ajaxSuccess', function() {
+  
+  //jquery Zoom Plugin
+  $('.img-zoom').elevateZoom(
+  	  );
 });
