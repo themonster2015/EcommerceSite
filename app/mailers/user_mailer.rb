609c2c7c5cea61ff86c08f122a5e8356d119459c
themonster2015 <quynhyen.vothi@gmail.com>
2017-07-18 22:39:56 +0200

    added styling,added automated email sending on signing up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,12 @@ class UserMailer < ApplicationMailer
   def contact_form(email, name, message)
   @message = message
     mail(:from => email,
-        :to => 'your-email@example.com',
+        :to => 'quynhyen.vothi@gmail.com',
         :subject => "A new contact form message from #{name}")
+  end
+  def welcome(user)
+  @appname = "Dermastore"
+  mail( :to => user.email,
+        :subject => "Welcome to #{@appname}!")
   end
 end
