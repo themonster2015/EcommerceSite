5a9ff3aef0696b9767a10a758289f2712cd0ae39
themonster2015 <quynhyen.vothi@gmail.com>
2017-07-18 20:09:38 +0200

    added contact form and set up mailer, sendgrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,37 @@
 class StaticPagesController < ApplicationController
-  def index
+  def about
   end
-  def landing_page
-    @products = Product.limit(3)
+
+  def contact
   end
-end
+
+   def landing_page
+    # if params[:q]
+    # search_term = params[:q]
+    # @products = Product.where("name LIKE ?", "%#{search_term}%")
+    # # return our filtered list here
+    # else
+    #   @products = Product.limit(3)
+    # end
+    if params[:q]
+       search_term = params[:q]
+       if (Rails.env == "production")
+       @products = Product.where("name ilike ?", "%#{search_term}%")
+      else
+        @products = Product.where("name LIKE ?", "%#{search_term}%")
+      end
+    else
+
+        @products = Product.all
+    end
+  end
+  
+   
+
+   def thank_you
+	  @name = params[:name]
+	  @email = params[:email]
+	  @message = params[:message]
+	  UserMailer.contact_form(@email, @name, @message).deliver_now
+	 end
+end 
